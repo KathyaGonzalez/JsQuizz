15dcc8d47d12697ec80d1fca9dd8d3c0b1404692
KathyaGonzalez <gonzco32@gmail.com>
2024-10-30 21:34:14 -0600

    Check answer

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,7 +4,22 @@ import { type Question as QuestionType } from "../types"
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { androidstudio} from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
+const showCheck = (index: number, info: QuestionType) => {
+    const {userSelectedAnswer, correctAnswer} = info
+    if(userSelectedAnswer== null) return 'transparent'
+    else if(index !== correctAnswer && index !==userSelectedAnswer) return 'transparent'
+    else if(index === correctAnswer) return 'green'
+    else if(index !== correctAnswer) return 'red'
+    return 'transparent'
+}
+
 const Question = ({info}: {info: QuestionType}) => {
+    const selectAnswer = useQuestionsStore(state => state.selectAnswer)
+
+    const createHeandleClick = (answerIndex: number) => () => {
+        selectAnswer(info.id, answerIndex)
+    }
+
     return (
         <Card variant='outlined' sx={{ bgcolor: '#222', p:2, marginTop: 4}}>
             
@@ -19,8 +34,12 @@ const Question = ({info}: {info: QuestionType}) => {
             <List sx={{bgcolor: '#333'}} disablePadding>
                 {
                     info.options.map((answer, index) => (
-                        <ListItem key={index} disablePadding divider >
-                            <ListItemButton>
+                        <ListItem key={index} disablePadding divider>
+                            <ListItemButton 
+                                disabled={info.userSelectedAnswer!=null}
+                                onClick={createHeandleClick(index)} sx={{
+                                backgroundColor: showCheck(index, info)
+                                }}>
                                 <ListItemText primary={answer}/>
                             </ListItemButton>
                         </ListItem>
--- src/store/questions.ts
+++ src/store/questions.ts
@@ -4,7 +4,8 @@ import { type Question } from '../types'
 interface State {
     questions: Question[]
     currentQuestion: number,
-    fetchQuestions: (limit: number) => Promise<void>
+    fetchQuestions: (limit: number) => Promise<void>,
+    selectAnswer: (questionId: number, answerIndex: number) => void
 }
 
 export const useQuestionsStore = create<State>((set, get) => {
@@ -16,6 +17,27 @@ export const useQuestionsStore = create<State>((set, get) => {
             const json = await res.json()
             const questions = json.sort(() => Math.random() -0.5).slice(0, limit)
             set({questions})
+        },
+        selectAnswer: (questionId: number, answerIndex: number) => {
+            const { questions } = get()
+            // deeo clone object 
+            const newQuestions = structuredClone(questions)
+            // find index question
+            const questionIndex = newQuestions.findIndex(q => q.id === questionId)
+            // find object question
+            const questionInfo = newQuestions[questionIndex]
+            // compare user answer and correct answer
+            const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
+
+            newQuestions[questionIndex] = {
+                ...questionInfo,
+                isCorrectUserAnswer,
+                userSelectedAnswer: answerIndex
+            }
+            
+            // update state
+            set({questions: newQuestions})
+
         }
     }
 
